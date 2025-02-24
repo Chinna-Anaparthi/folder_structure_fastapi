598a710c148e2f15109643227aada9d39def080e
Chinna Anaparthi <canaparthi@msa.miraclesoft.com>
2025-02-24 20:56:08 +0530

    added books api2/24

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class BookCreateModel(BaseModel):
+    title:str   
+    author:str
 
+book = [
+    {
+        "id":1,
+        "title":"think python",
+        "author":"chinna"
+    },
+    {
+        "id":2,
+        "title":"think cython",
+        "author":"chinna"
+    }
+]
 @app.post("/items/", status_code=201)
 async def create_item(name: str):
     return {"name": name}
@@ -10,3 +26,14 @@ async def create_item(name: str):
 @app.post("/app/{item}",status_code=201)
 async def read_item(item:str):
     return {"message":f"hello {name}"}
+
+@app.post("/api/create_book",status_code=201)
+async def create_book(book_data:BookCreateModel):
+    return {
+        "title":book_data.title,
+        "author":book_data.author
+    }
+
+@app.get("/app/books",status_code=201)
+async def get_all_books():
+    return book
