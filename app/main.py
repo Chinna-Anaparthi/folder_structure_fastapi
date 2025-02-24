4a4ad92ed8af34a599580cc7296752b36e3d9b24
Chinna Anaparthi <canaparthi@msa.miraclesoft.com>
2025-02-24 20:16:27 +0530

    new dev code

--- app/main.py
+++ app/main.py
@@ -3,8 +3,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/items/", status_code=201)
+async def create_item(name: str):
+    return {"name": name}
 
-
+@app.post("/app/{item}",status_code=201)
+async def read_item(item:str):
+    return {"message":f"hello {name}"}
